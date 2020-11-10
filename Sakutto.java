86f6d77f5a261a38834ee7257206e0a6010b941d
kurea66 <passionately.66.meltdown.4949@gmail.com>
2020-11-10 20:29:11 +0900

    Sakutto追加

---
+++ Sakutto.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sakutto{
+	public static void main(String[] args){
+		char[] hello={'H','E','L','L','O'};
+		for(int i=0;i<5;i++){
+			System.out.print(hello[hello.length-i-1]);
+		}
+	}
+}
