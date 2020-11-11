66091b071c723b416a3a83ea2d3054719e2d05e5
kurea <passionately.66.meltdown.4949@gmail.com>
2020-11-11 14:18:11 +0900

    Sakutto更新

--- Sakutto.java
+++ Sakutto.java
@@ -2,8 +2,13 @@ import java.util.*;
 public class Sakutto{
 	public static void main(String[] args){
 		char[] hello={'H','E','L','L','O'};
-		for(int i=0;i<5;i++){
-			System.out.print(hello[hello.length-i-1]);
+		for(int i=0;i<hello.length/2;i++){
+			char temp = 0;
+			temp=hello[i];
+			hello[i]=hello[hello.length-1-i];
+			hello[hello.length-1-i]=temp;
+			//System.out.print(hello[hello.length-i-1]);
 		}
+		System.out.println(Arrays.toString(hello));
 	}
 }
